b0f46c83910d80dc3ecedee9173792d5fe4d0b98
SunDawning <dpmeichen@gmail.com>
2020-10-10 22:06:50 +0800

    修复 强制使用英文的星期

--- insert-time-stamp-timestamp.el
+++ insert-time-stamp-timestamp.el
@@ -3,10 +3,12 @@
 
 \"<%Y-%m-%d %a %H:%M:%S UTC%:z>\"
 
-- 如果当前光标处于时间戳，那么更新时间戳里的时间"
+- 如果当前光标处于时间戳，那么更新时间戳里的时间
+- 强制使用英文的星期"
   (interactive)
   (when (and (fboundp 'org-at-timestamp-p)
              (org-at-timestamp-p))
     (replace-match ""))
   (insert
-   (format-time-string "<%Y-%m-%d %a %H:%M:%S UTC%:z>")))
+   (let ((system-time-locale "ENU"))
+     (format-time-string "<%Y-%m-%d %a %H:%M:%S UTC%:z>"))))
